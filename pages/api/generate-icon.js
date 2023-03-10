ee2bfff83dc4d154a887b4ac4f7093f81060e356
nasir456244 <arturlegendflame@gmail.com>
2023-03-10 13:58:54 +1000

    change size

--- pages/api/generate-icon.js
+++ pages/api/generate-icon.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     const response = await openai.createImage({
       prompt: `generate ${text} icon that is ${selectedColor} color, ${selectShape === "square" ? "rounded square" : selectShape} shape, cool ${selectStyle} style and a background ${selectedColor} color`,
       n: 1,
-      size: "256x256",
+      size: "1024x1024",
     });
     const image_url = response?.data?.data[0]?.url;
     res.status(200).json({image_url})
